39e79df583ed383e0426e0ce9548bebc50b6a13c
Zach <zachlandis91@gmail.com>
2023-01-17 13:06:52 -0700

    commit tree c1f5015ab22d7172349712651fa0297d9ab405d8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Cat {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function() {
+            return (`${this.name} says meow!`)
+        }
+    }
+}
+
+
+
+class Dog {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            return `${this.name} says woof!`
+        }
+    }
+}
+
+
+class Bird {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            if (this.sex === 'male') {
+                return `It\'s me! ${this.name}, the parrot!`
+            } else {
+                return `${this.name} says squawk!`
+            }     
+        }
+    }
+}
+
